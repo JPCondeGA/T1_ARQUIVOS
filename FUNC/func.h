edb78ba1bc4072e3898e52a705faf829894b9f1d
E-2-B <eduardo2b@usp.br>
2026-04-12 19:35:18 +0000

    Atualiza comentários

--- FUNC/func.h
+++ FUNC/func.h
@@ -12,19 +12,19 @@
     /* Realiza a funcionalidade [1] (CREATE TABLE) por meio de funções auxiliares. Inicializa valores e executa operações de entrada e saída. Acessível do arquivo main.c do projeto. */
     void func_create_interface();
 
-    /**/
+    /* Realiza a funcionalidade [2] (SELECT) por meio de funções auxiliares. Inicializa valores e executa operações de entrada e saída. Acessível do arquivo main.c do projeto. */
     void func_select_interface();
 
     /* Realiza a funcionalidade [3] (SELECT FROM WHERE) por meio de funções auxiliares. Inicializa valores e executa operações de entrada e saída. Acessível do arquivo main.c do projeto. */
     void func_select_where_interface();    
     
-    /**/
+    /* Realiza a funcionalidade [4] (DELETE) por meio de funções auxiliares. Inicializa valores e executa operações de entrada e saída. Acessível do arquivo main.c do projeto. */
     void func_delete_interface();
     
-    /**/
+    /* Realiza a funcionalidade [5] (INSERT INTO) por meio de funções auxiliares. Inicializa valores e executa operações de entrada e saída. Acessível do arquivo main.c do projeto. */
     void func_insert_interface();
     
-    /**/
+    /* Realiza a funcionalidade [6] (UPDATE) por meio de funções auxiliares. Inicializa valores e executa operações de entrada e saída. Acessível do arquivo main.c do projeto. */
     void func_update_interface();
 
 #endif
